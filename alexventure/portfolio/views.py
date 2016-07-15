45965ef4e9f9ba6315e43b47ae19d34505dde868
Alex Albino <webmaster@alexventure.com>
2016-07-14 17:25:13 -0700

    Adding first revision of text

--- alexventure/portfolio/views.py
+++ alexventure/portfolio/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello");
+    template = loader.get_template('portfolio/index.html')
+    context = {}
+    return HttpResponse(template.render(context, request));
