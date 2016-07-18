bd7e1178b30298fc39d1a7ce26b0b219b95aaee3
Alex Albino <webmaster@alexventure.com>
2016-07-18 15:54:57 -0700

    Updating settings, static file settings

--- alexventure/alexventure/urls.py
+++ alexventure/alexventure/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('portfolio.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] 
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
